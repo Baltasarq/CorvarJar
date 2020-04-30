f647ad7b204f974caf1c7f64bcbb5975311ee6af
baltasar <baltasarq@gmail.com>
2020-04-30 12:11:51 +0200

    Improved copyright message from the test program

--- src/com/devbaltasarq/corvarjartest/CorvarJarTest.java
+++ src/com/devbaltasarq/corvarjartest/CorvarJarTest.java
@@ -5,7 +5,7 @@ import com.devbaltasarq.corvarjar.ResultAnalyzer;
 public class CorvarJarTest {
     public static void main(String[] args)
     {
-        System.out.println( "CorvarJarTest (c) 2020 Baltasar MIT License <jbgarcia@uvigo.es>" );
+        System.out.println( "CorvarJarTest (c) 2020 Baltasar Mile Group MIT License <jbgarcia@uvigo.es>" );
 
         if ( args.length > 0 ) {
             ResultAnalyzer resultAnalyzer = new ResultAnalyzer( args[ 0 ] );
